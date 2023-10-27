fd8b14f7e839b52eae2e6fb46f78d5cd88e08c9b
AlvianyAdzanyFebriyanti <alviany1818@gmail.com>
2023-10-27 18:29:06 +0700

    Biodata Laravel

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,14 @@
+@extends('header')
+
+
+@section('content')
+<div align="center">
+  
+  <img src= {{asset('image/Profile.jpeg')}} alt="" width="150px" height="150px" style="border-radius: 100px; -moz-border-radius: 300px;">
+ </div> 
+ <br>
+ <h4>About Me</h4>  
+ <p>Saya adalah Alviany Adzany Febriyanti, seorang mahasiswi yang berasal dari Bekasi dan sedang menempuh pendidikan di Universitas Singaperbangsa Karawang. Saya memiliki ketertarikan yang kuat dalam bidang pengembangan website, database, dan aplikasi mobile. Dalam perjalanan pendidikan dan pengembangan karier saya, saya telah meraih sertifikasi Cisco yang menunjukkan kompetensi dalam jaringan dan juga telah berpengalaman sebagai Junior Web Developer. Saya merasa senang dengan teknologi informasi dan terus bersemangat untuk belajar dan tumbuh dalam industri ini. Saya yakin bahwa pengalaman dan minat saya dalam berbagai aspek teknologi informasi akan memberikan kontribusi yang berarti dalam karier saya di masa depan.</p>
+@endsection
+
+@extends('footer')
